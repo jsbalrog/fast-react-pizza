5d85c15f5b8c2b3488d8a99594e192f613116884
jsbalrog <ted.jenkins@familysearch.org>
2024-01-29 08:54:50 -0700

    initial commit

---
+++ src/ui/LinkButton.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link, useNavigate } from "react-router-dom"
+
+export default function LinkButton({ to, children }) {
+  const navigate = useNavigate()
+  const classes = "text-sm text-blue-500 hover:text-blue-600 hover:underline"
+
+  if (to === "-1")
+    return (
+      <button className={classes} onClick={() => navigate(-1)}>
+        {children}
+      </button>
+    )
+  return (
+    <Link to={to} className={classes}>
+      {children}
+    </Link>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-anonymous-default-export */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: "Roboto Mono, monospace",
+      },
+      height: {
+        screen: "100dvh",
+      },
+    },
+  },
+  plugins: [],
+}
